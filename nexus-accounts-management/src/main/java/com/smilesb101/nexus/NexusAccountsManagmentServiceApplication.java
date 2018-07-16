b69743b108decb27b40d1eb14ce1dc8ccdeb8d15
SmileSB101 <zxb13194889618@outlook.com>
2018-07-16 23:29:21 +0800

    init commit

---
+++ nexus-accounts-management/src/main/java/com/smilesb101/nexus/NexusAccountsManagmentServiceApplication.java
@@ -0,0 +1,14 @@
+package com.smilesb101.nexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class NexusAccountsManagmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusAccountsManagmentServiceApplication.class, args);
+    }
+}
---
+++ nexus-accounts-management/src/test/java/com/smilesb101/nexus/NexusAccountsManagmentServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.smilesb101.nexus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NexusAccountsManagmentServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
