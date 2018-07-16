0aa02e621f983d8daeed1b0ce5ef8aa7b0382b2b
SmileSB101 <zxb13194889618@outlook.com>
2018-07-16 23:52:13 +0800

    add common config

--- nexus-register-center/src/main/java/com/smilesb101/nexus/NexusRegistCenterApplication.java
+++ nexus-register-center/src/main/java/com/smilesb101/nexus/NexusRegistCenterApplication.java
@@ -2,14 +2,13 @@ package com.smilesb101.nexus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class NexusApplication {
+public class NexusRegistCenterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NexusApplication.class, args);
+        SpringApplication.run(NexusRegistCenterApplication.class, args);
     }
 }
--- nexus-register-center/src/test/java/com/smilesb101/nexus/NexusRegistCenterApplicationTests.java
+++ nexus-register-center/src/test/java/com/smilesb101/nexus/NexusRegistCenterApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class NexusApplicationTests {
+public class NexusRegistCenterApplicationTests {
 
     @Test
     public void contextLoads() {
